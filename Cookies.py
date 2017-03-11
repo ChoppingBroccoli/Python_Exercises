1cfc1f1c32a8aaffaf07ec207bf397375f00855c
Justin <jneezy2008@gmail.com>
2017-03-11 00:08:18 -0500

    Making it more fun
    Added mroe code to Cookies.py
    Fixed typo in get_input_from_user.py

--- Cookies.py
+++ Cookies.py
@@ -1,7 +1,39 @@
-user_response=int(input("How many cookies do you want? "))
-cookies_eaten=0
-while user_response > 0:
-	user_response = user_response - 1
-	cookies_eaten=cookies_eaten + 1
-	print ("You have eaten",cookies_eaten, "cookies!")
-print ("All out of cookies!")
+import random
+cookies_available = random.randint(0,9) #Generate a random number of cookies
+print("There are", cookies_available, "cookies available.") #Tell user how many cookies are available
+
+cookies_wanted = int(input("How many would you like? ")) #Ask user how many cookies they want
+
+while cookies_available > 0:
+    if cookies_wanted > cookies_available:
+        print("We don't have that many cookies")
+        break
+
+    elif cookies_wanted == cookies_available:
+        print("That's all the cookies")
+        break
+    elif cookies_wanted < cookies_available:
+        cookies_available = cookies_available - cookies_wanted
+        print("There are now",cookies_available,"cookies remaining.")
+
+
+
+"""""
+######################################
+########### Psuedo Code ##############
+######################################
+While there are more than 0 cookies in the jar, keep eating cookies.
+
+If there are still cookies in the jar tell the user how many cookies there are then ask them if they want to continue eating. If yes, how many would they like to eat?
+
+1) generate random number between 1 and 100
+2) Tell user how many cookies there are
+3) User inputs amount of cookies they want to eat
+	a) if number is < cookies available, show user they ate the cookies
+		i) show user how many cookies remain.
+		ii) ask user if they want to eat more cookies.
+		iii) ask user how many more cookies they want to eat.
+	c) if number is = cookies avaible, tell user they ate all the cookies.
+	d) if number is > cookies available, tell user we don't have that many cookies
+This is a comment block
+"""""
--- get_input_from_user.py
+++ get_input_from_user.py
@@ -1,2 +1,2 @@
 x=input("What is your name? ")
-print("Glad to meet your",x,"!")
+print("Glad to meet you",x,"!")
