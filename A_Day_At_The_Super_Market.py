0db701028968f312927bf1764be511a6610e76fb
jneezy2008@gmail.com <jneezy2008@gmail.com>
2017-04-09 19:51:37 -0400

    Confirmed working code. Not sure why '%s' and '%' are needed in the last 2 lines but they are.

--- A_Day_At_The_Super_Market.py
+++ A_Day_At_The_Super_Market.py
@@ -3,5 +3,5 @@ stock = {"banana": 6, "apple": 0, "orange": 32, "pear": 15}
 
 for each_fruit in prices:
     print (each_fruit)
-    print "price: %s" % prices[each_fruit]
+    print "price: %s" % prices[each_fruit] #not sure why 'price' must be lower case but it does. Not sure what "%s" or "%" are needed.
     print "stock: %s" % stock[each_fruit]
