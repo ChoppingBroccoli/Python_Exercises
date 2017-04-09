01ee47ed3aceafa7090ccdbb94f171c2ebfaad1b
jneezy2008@gmail.com <jneezy2008@gmail.com>
2017-04-09 19:49:21 -0400

    Confirmed working code. Not sure why '%s' and '%' are needed in the last 2 lines but they are.

---
+++ A_Day_At_The_Super_Market.py
@@ -0,0 +1,7 @@
+prices = {"banana": 4, "apple": 2, "orange": 1.5, "pear": 3}
+stock = {"banana": 6, "apple": 0, "orange": 32, "pear": 15}
+
+for each_fruit in prices:
+    print (each_fruit)
+    print "price: %s" % prices[each_fruit]
+    print "stock: %s" % stock[each_fruit]
